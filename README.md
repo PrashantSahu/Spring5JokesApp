86196cec028b0fbdb9cb18d419af936704912f44
prsahu <prsahu@expedia.com>
2018-08-29 22:17:26 +0530

    All done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Spring5JokesApp
+
+Extremely simple app using service, controller and thymeleaf template as a view.
---
+++ src/main/java/com/prashant/joke/jokeapp/services/JokeService.java
@@ -0,0 +1,6 @@
+package com.prashant.joke.jokeapp.services;
+
+public interface JokeService {
+
+    String getJoke();
+}
