32e6313b9d200b2385409e7a91d7d4aa5815fe7d
prsahu <prsahu@expedia.com>
2018-08-31 22:53:34 +0530

    Using bean instead

---
+++ src/main/java/com/prashant/joke/jokeapp/config/ChuckConfiguration.java
@@ -0,0 +1,16 @@
+package com.prashant.joke.jokeapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes(){
+        return new ChuckNorrisQuotes();
+    }
+
+}
